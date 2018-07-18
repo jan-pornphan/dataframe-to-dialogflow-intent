5df135ae0b660aac99689c453c56af8fe0287508
Pornphan Akkaragumtorn <pornphanakkaragumtorn@Pornphans-MacBook-Pro.local>
2018-07-18 12:09:13 +0700

    facebook and default message handler

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from intent_builder import IntentBuilder
 import os
 import uuid
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialogflow-sdk-test-f7fbd9c66588.json"
-project_id = 'dialogflow-sdk-test'
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialogflow-sdk-test-f7fbd9c66588.json"
+# project_id = 'dialogflow-sdk-test'
+project_id = 'testagent-379ff'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="testagent-379ff-d76830023068.json"
 session_id = uuid.uuid4()
 
 df = pd.read_csv('BookTest.csv')
--- message_handler.py
+++ message_handler.py
@@ -8,35 +8,48 @@ platform_type = {
 }
 
 
-def handle_message(response, response_type, platform):    
+def handle_message(response, response_type, platform):
+    if platform == 'facebook':    
+        return _handle_facebook_message(response, response_type)
+    return _handle_default_message(response, response_type)
+
+def _handle_facebook_message(response, response_type):
     if response_type == 'quick reply':
-        return _handle_quick_reply(response, platform)
+        return _handle_quick_reply(response)
     elif response_type == 'card':
-        return _handle_card(response, platform)
+        return _handle_card(response)
     elif response_type == 'payload':
-        return _handle_custom_payload(response, platform)
+        return _handle_facebook_custom_payload(response)
+    return _handle_facebook_text(response)
 
-    return _handle_text(response, platform)
-
-
-def _handle_text(response, platform):
+def _handle_default_message(response, response_type):
+    if response_type == 'payload':
+        return _handle_default_custom_payload(response)
+    return _handle_default_text(response)
+        
+def _handle_facebook_text(response):
     return Intent.Message(
-        platform=platform_type[platform],
+        platform=platform_type['facebook'],
         text=Intent.Message.Text(text=response.split(','))
     )
 
+def _handle_default_text(response):
+    return Intent.Message(
+        platform=platform_type['default'],
+        text=Intent.Message.Text(text=response.split(','))
+    )
 
-def _handle_quick_reply(response, platform):
+def _handle_quick_reply(response):
     obj = json.loads(response)
     return Intent.Message(
-        platform=platform_type[platform],
+        platform=platform_type['facebook'],
         quick_replies=Intent.Message.QuickReplies(
             title=obj['title'],
             quick_replies=obj['quick_replies'].split(',')
         )
     )
 
-def _handle_card(response, platform):
+def _handle_card(response):
     obj = json.loads(response)
     button_texts = obj['button'].split(',')
     buttons = []
@@ -45,7 +58,7 @@ def _handle_card(response, platform):
         buttons.append(text)
 
     return Intent.Message(
-        platform=platform_type[platform],
+        platform=platform_type['facebook'],
         card=Intent.Message.Card(
             title=obj['title'],
             subtitle=obj['subtitle'],
@@ -54,13 +67,22 @@ def _handle_card(response, platform):
         )
     )
 
-
-def _handle_custom_payload(response, platform):
+def _handle_facebook_custom_payload(response):
     obj = json.loads(response)
     payload = Struct()    
-    payload[platform] = obj
+    payload['facebook'] = obj
     print(payload)
     return Intent.Message(
-        platform = platform_type[platform],
+        platform = platform_type['facebook'],
+        payload = payload
+    )
+
+def _handle_default_custom_payload(response):
+    obj = json.loads(response)
+    payload = Struct()    
+    payload['line'] = obj
+    print(payload)
+    return Intent.Message(
+        platform = platform_type['default'],
         payload = payload
     )
